5c44be55fe5dada6bcd3cb404137f8682b5fda6a
bwlp <42607217+bwlp@users.noreply.github.com>
2023-02-04 14:37:26 +0900

    feat: improve question alert

---
+++ src/lib/QuestionAlert.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Close from "carbon-icons-svelte/lib/Close.svelte";
+  import Redo from "carbon-icons-svelte/lib/Redo.svelte";
+  export let numberOfQuestions;
+  export let question;
+</script>
+
+{#if question}
+  <div class="alert" role="alert">
+    Jump to Question
+    {question}/{numberOfQuestions}
+    <div class="ml-6 inline-flex flex-col items-center text-xs">
+      <Close />
+      Del/Esc
+      <div class="rotate-180 mt-2">
+        <Redo className="-rotate-180" />
+      </div>
+      Enter
+    </div>
+  </div>
+{/if}
--- src/lib/RadioForm.svelte
+++ src/lib/RadioForm.svelte
@@ -1,36 +1,50 @@
 <script lang="ts">
+  import QuestionAlert from "./QuestionAlert.svelte";
   import RadioGroup from "./RadioGroup.svelte";
   import { fieldsetBindings } from "./stores";
 
-  let numberOfOptions: number = 6;
-  let numberOfQuestions: number = 20;
+  let numberOfOptions: number = 4;
+  let numberOfQuestions: number = 40;
   const questionArray = Array(numberOfQuestions);
   let question: string = "";
   const handleKeyPress = (e: KeyboardEvent) => {
     if (/[0-9]/.test(e.key)) {
       question = question.concat(e.key);
+      if (parseInt(question) > numberOfQuestions) {
+        question = numberOfQuestions.toString();
+      }
     } else if (/Delete|Escape/.test(e.key)) {
       question = "";
     } else if (e.key === "Backspace") {
       question = question.slice(0, -1);
     } else if (e.key === "Enter") {
-      if (parseInt(question) > numberOfQuestions) {
-        question = numberOfQuestions.toString();
-      } else {
-        fieldsetBindings[parseInt(question) - 1].focus();
+      fieldsetBindings[parseInt(question) - 1]?.focus();
+      question = "";
+    } else if (/ArrowUp|ArrowLeft/.test(e.key)) {
+      if (Object.values(fieldsetBindings).includes(document.activeElement)) {
+        e.preventDefault();
+        const prevIndex = Math.max(
+          Object.values(fieldsetBindings).indexOf(document.activeElement) - 1,
+          0
+        );
+        fieldsetBindings[prevIndex].focus();
+      }
+    } else if (/ArrowDown|ArrowRight/.test(e.key)) {
+      if (Object.values(fieldsetBindings).includes(document.activeElement)) {
+        e.preventDefault();
+        const nextIndex = Math.min(
+          Object.values(fieldsetBindings).indexOf(document.activeElement) + 1,
+          numberOfQuestions - 1
+        );
+        fieldsetBindings[nextIndex].focus();
       }
     }
   };
 </script>
 
-{#if question}
-  <div class="alert" role="alert">
-    Jump to Question
-    {question}
-  </div>
-{/if}
+<QuestionAlert {numberOfQuestions} {question} />
 
-<section class="grid-cols-3 grid border-collapse" on:keydown={handleKeyPress}>
+<section class="grid-cols-4 grid border-collapse" on:keydown={handleKeyPress}>
   {#each questionArray as _, index}
     <RadioGroup questionIndex={index} {numberOfOptions} />
   {/each}
